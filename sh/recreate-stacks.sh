daf5db3947cf0e492ade2ef0fd802b5274b671cd
cypherix93 <bikram.ghosh93@gmail.com>
2022-11-26 17:04:34 -0500

    recreate stacks improved

--- sh/recreate-stacks.sh
+++ sh/recreate-stacks.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
+# arguments
+directory="*";
+force="";
+
+while getopts d:f flag
+do
+    case "${flag}" in
+        d) directory=${OPTARG};;
+        f) force="--force-recreate";;
+    esac
+done
+
+compose_dirs=$(realpath -- "$(dirname -- "$0")/../compose/$directory/");
+
+#echo "$directory";
+#echo "$force";
+#echo "$compose_dirs";
+#
+#ls -ld $compose_dirs;
+
+#exit 0;
+
 # recreate all stacks in "compose" directory
-ls -d $(realpath -- "$(dirname -- "$0")/../compose/*/") | \
+ls -d $compose_dirs | \
     while read -r dir; do \
         echo "✨ Recreating Stack: $(basename -- $dir)"
         echo "   📁 Directory: $dir"
-        docker compose --project-directory $dir up -d;
+        docker compose --project-directory $dir up -d --pull 'always' $force;
     done
